1237d8b79e69e5de83aab94cdb0577585a048b54
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-03-17 21:25:39 -0700

    Header | #9.1

---
+++ src/Components/Header.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import Search from "../Routes/Search";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: black;
+  font-size: 12px;
+  padding: 20px 60px;
+  color: white;
+`;
+
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.svg`
+  margin-right: 50px;
+  width: 95px;
+  height: 25px;
+`;
+
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  margin-right: 20px;
+`;
+
+function Header() {
+  return (
+    <Nav>
+      <Col>
+        <Logo />
+        <Items>
+          <Item>Home</Item>
+          <Item>Tv Shows</Item>
+        </Items>
+      </Col>
+      <Col>
+        <Search />
+      </Col>
+    </Nav>
+  );
+}
+
+export default Header;

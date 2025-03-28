2a85df15853f5cf5566084d977cf640fb227e743
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-03-27 23:35:33 -0700

    Slider part two | #9.8

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -8,6 +8,7 @@ import useWindowDimensions from "../windowDimensions";
 
 const Wrapper = styled.div`
   background-color: black;
+  padding-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -50,11 +51,14 @@ const Row = styled(motion.div)`
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-color: #fff;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   height: 200px;
-  color: red;
   font-size: 66px;
+  gap: 5px;
 `;
 
 const rowVariants = {
@@ -69,6 +73,8 @@ const rowVariants = {
   },
 };
 
+const offset = 6;
+
 function Home() {
   const width = useWindowDimensions();
   const { data, error, isLoading } = useQuery<IGetMoviesResult>({
@@ -77,7 +83,17 @@ function Home() {
   });
 
   const [index, setIndex] = useState(0);
-  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   console.log(data, isLoading);
   return (
     <Wrapper>
@@ -93,7 +109,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial={{ x: width + 10 }}
@@ -102,9 +118,15 @@ function Home() {
                 transition={{ type: "tween", duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map((i) => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    />
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
